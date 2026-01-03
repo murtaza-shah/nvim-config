98904ae6b567d04cb518beef8269cf3164a491de
murtaza-shah <shahmurtaza5@gmail.com>
2026-01-03 00:44:20 -0700

    completions and more ergonomic lsp with mason :)

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
--- vim.o is shorthand for vim.opt
+-----------------------------------------------------------
+-- Basic options
+-----------------------------------------------------------
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.wrap = false
@@ -8,33 +10,188 @@ vim.o.signcolumn = "yes"
 vim.o.winborder = "rounded"
 vim.g.mapleader = " "
 
--- syntax: mode, keybind, command
-vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+-----------------------------------------------------------
+-- Keymaps (core)
+-----------------------------------------------------------
+-- Reload current config file
+vim.keymap.set('n', '<leader>o', ':update<CR>:source<CR>')
+
+-- Save / quit
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
 
-vim.keymap.set('v', '<leader>y', '"+yy')
+-- Clipboard in visual
+vim.keymap.set('v', '<leader>y', '"+y')
 vim.keymap.set('v', '<leader>p', '"+P')
 
--- built-in package manager
+-----------------------------------------------------------
+-- Plugin management (built-in vim.pack)
+-----------------------------------------------------------
 vim.pack.add({
-	{ src = "https://github.com/vague2k/vague.nvim" },
-	{ src = "https://github.com/stevearc/oil.nvim" },
-	{ src = "https://github.com/echasnovski/mini.pick" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
+  -- UI / theme / basic tools
+  { src = "https://github.com/vague2k/vague.nvim" },
+  { src = "https://github.com/stevearc/oil.nvim" },
+  { src = "https://github.com/echasnovski/mini.pick" },
+
+  -- LSP server definitions (no direct API use)
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+
+  -- LSP installer / manager
+  { src = "https://github.com/williamboman/mason.nvim" },
+  { src = "https://github.com/williamboman/mason-lspconfig.nvim" },
+
+  -- Completion engine + sources
+  { src = "https://github.com/hrsh7th/nvim-cmp" },
+  { src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
+  { src = "https://github.com/hrsh7th/cmp-buffer" },
+  { src = "https://github.com/hrsh7th/cmp-path" },
+
+  -- Snippets
+  { src = "https://github.com/L3MON4D3/LuaSnip" },
+  { src = "https://github.com/saadparwaiz1/cmp_luasnip" },
 })
 
-require "mini.pick".setup()
-require "oil".setup()
+-----------------------------------------------------------
+-- Basic plugin setup
+-----------------------------------------------------------
+require("mini.pick").setup()
+require("oil").setup()
+
+-- mini.pick keymaps
 vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
 vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
 vim.keymap.set('n', '<leader>e', ":Oil<CR>")
 
-vim.lsp.enable({
-	"lua_ls"
-})
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
-
+-- Colorscheme
 vim.cmd("colorscheme vague")
--- make statusline not have a bg color
-vim.cmd(":hi statusline guibg=NONE")
+-- Make statusline have no background
+vim.cmd("hi StatusLine guibg=NONE")
+
+-----------------------------------------------------------
+-- Mason: LSP installer / manager
+-----------------------------------------------------------
+require("mason").setup()
+
+local mason_lspconfig = require("mason-lspconfig")
+
+mason_lspconfig.setup({
+  -- Install whatever you want from the Mason UI, no fixed list.
+  automatic_installation = true,
+})
+
+-- Open Mason UI to see installed / installable LSPs
+vim.keymap.set('n', '<leader>lm', ':Mason<CR>', { desc = "Open Mason (LSP manager)" })
+
+-----------------------------------------------------------
+-- Completion: nvim-cmp + LuaSnip
+-----------------------------------------------------------
+vim.o.completeopt = "menu,menuone,noselect"
+
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<C-e>"] = cmp.mapping.abort(),
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },  -- LSP completion
+    { name = "luasnip" },   -- snippets
+  }, {
+    { name = "buffer" },    -- words already in this buffer
+    { name = "path" },      -- filesystem paths
+  }),
+})
+
+-----------------------------------------------------------
+-- LSP configuration (terse / auto style)
+-----------------------------------------------------------
+-- Load lspconfig so server definitions are registered.
+pcall(require, "lspconfig")
+
+-- Global defaults for all LSPs
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+vim.lsp.config("*", {
+  capabilities = capabilities,
+})
+
+-- Extra config for specific servers (optional)
+-- Example: Lua LS – don't warn about `vim` and skip 3rd party checks.
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        checkThirdParty = false,
+      },
+    },
+  },
+})
+
+-- Function to enable all LSPs that Mason has installed
+local function enable_installed_lsps()
+  local servers = mason_lspconfig.get_installed_servers()
+  for _, server_name in ipairs(servers) do
+    vim.lsp.enable(server_name)
+  end
+end
+
+-- Enable any already-installed servers at startup
+enable_installed_lsps()
+
+-- Keymap to re-run that after installing new servers in Mason
+vim.keymap.set('n', '<leader>la', function()
+  enable_installed_lsps()
+  print("Enabled installed LSP servers")
+end, { desc = "Enable installed LSPs" })
+
+-- Format via LSP
+vim.keymap.set('n', '<leader>lf', function()
+  vim.lsp.buf.format({ async = true })
+end, { desc = "LSP format buffer" })
+
+-- Show which LSPs are attached to the current buffer
+vim.keymap.set('n', '<leader>ll', function()
+  local clients = vim.lsp.get_clients({ bufnr = 0 })
+  if vim.tbl_isempty(clients) then
+    print("No LSP attached to this buffer")
+    return
+  end
+  local names = {}
+  for _, c in ipairs(clients) do
+    table.insert(names, c.name)
+  end
+  print("LSP clients: " .. table.concat(names, ", "))
+end, { desc = "List LSP clients for current buffer" })
+
