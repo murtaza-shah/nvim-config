ea935a4341f0195ce0afd1d258490b6c9626a230
murtaza-shah <shahmurtaza5@gmail.com>
2026-01-03 00:22:49 -0700

    basic lsp, copy paste utils

--- init.lua
+++ init.lua
@@ -4,28 +4,37 @@ vim.o.relativenumber = true
 vim.o.wrap = false
 vim.o.tabstop = 4
 vim.o.swapfile = false
-
+vim.o.signcolumn = "yes"
+vim.o.winborder = "rounded"
 vim.g.mapleader = " "
 
 -- syntax: mode, keybind, command
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+
+vim.keymap.set('v', '<leader>y', '"+yy')
+vim.keymap.set('v', '<leader>p', '"+P')
 
 -- built-in package manager
 vim.pack.add({
-		{src = "https://github.com/vague2k/vague.nvim"},
-		{src = "https://github.com/stevearc/oil.nvim"},
-		{src = "https://github.com/echasnovski/mini.pick"},
-		{src = "https://github.com/neovim/nvim-lspconfig"},
+	{ src = "https://github.com/vague2k/vague.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
 })
+
+require "mini.pick".setup()
+require "oil".setup()
+vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
+vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
+vim.keymap.set('n', '<leader>e', ":Oil<CR>")
+
+vim.lsp.enable({
+	"lua_ls"
+})
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
 vim.cmd("colorscheme vague")
 -- make statusline not have a bg color
 vim.cmd(":hi statusline guibg=NONE")
-
--- lsp stuff
-vim.lsp.enable({
-		"lua_ls"
-})
