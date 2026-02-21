8b5a4d47a456191fae0e9bedaa99d66d268a86f9
murtaza-shah <shahmurtaza5@gmail.com>
2026-02-20 18:49:22 -0700

    show edited files

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,6 +15,50 @@ vim.keymap.set("v", "<leader>p", '"+P"')
 
 -- Buffer navigation
 vim.keymap.set("n", "<leader>bb", "<C-^>")
+vim.keymap.set("n", "<leader>be", function()
+  local ok, mini_pick = pcall(require, "mini.pick")
+  if not ok then
+    return
+  end
+
+  local target_win = vim.api.nvim_get_current_win()
+  local handle = io.popen("git status --porcelain 2>/dev/null")
+  if not handle then
+    return
+  end
+
+  local items = {}
+  for line in handle:lines() do
+    local status = line:sub(1, 2)
+    local filepath = line:sub(4)
+    local display = status .. " " .. filepath
+    table.insert(items, { text = display, filepath = filepath })
+  end
+  handle:close()
+
+  if #items == 0 then
+    vim.notify("No modified files", vim.log.levels.INFO)
+    return
+  end
+
+  mini_pick.start({
+    source = {
+      items = items,
+      choose = function(item)
+        vim.schedule(function()
+          if not item or not item.filepath then
+            return
+          end
+          if vim.api.nvim_win_is_valid(target_win) then
+            vim.api.nvim_set_current_win(target_win)
+          end
+          vim.cmd("edit " .. vim.fn.fnameescape(item.filepath))
+        end)
+      end,
+    },
+  })
+end, { desc = "List git modified files" })
+
 vim.keymap.set("n", "<leader>bl", function()
   local ok, mini_pick = pcall(require, "mini.pick")
   if not ok then
